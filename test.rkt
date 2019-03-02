fb892e615ebd71539219780f8c04422cd5b7b2fd
heycalmdown <heyacct@gmail.com>
2019-03-02 14:55:44 +0900

    retrive my status_text

--- test.rkt
+++ test.rkt
@@ -1,10 +1,17 @@
 #lang racket
-(require slack-api)
+; (require slack-api)
+(require "webapi.rkt" threading)
 
 (define app-token (getenv "APP_TOKEN"))
 (define bot-token (getenv "BOT_TOKEN"))
 (define channel (getenv "SLACK_CHANNEL"))
 
 (slack-api-test)
-(slack-chat-postMessage app-token channel "haha")
-(rtm-start bot-token)
+; (slack-users-setPresence app-token "auto")
+(let ([profile (slack-users-profile-get app-token)])
+     (~> profile
+         (hash-ref 'profile)
+         (hash-ref 'status_text)))
+; (slack-chat-postMessage app-token "general" "haha :pray:" #:as_user true #:username "babo")
+; (slack-users-list app-token)
+; (rtm-start bot-token)
